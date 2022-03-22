464a333b6c4c2674e25d489ce327a34d0d743240
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2022-03-22 12:34:08 -0300

    Add project files.

---
+++ LanchesM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
