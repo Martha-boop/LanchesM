31fe9823a283010c7d50d877837eda2b289a190e
Marta Neris <martaneris35@gmail.com>
2022-03-23 10:49:42 -0300

    commit

---
+++ LanchesM/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿namespace LanchesM.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+
+        // relacinamento para listar os lanches
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
