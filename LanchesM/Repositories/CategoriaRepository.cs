7479fd5ae02fe1ccc800b0ea741bd06903174bf4
Marta Neris <martaneris35@gmail.com>
2022-03-24 17:04:59 -0300

    commit

---
+++ LanchesM/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesM.Context;
+using LanchesM.Models;
+using LanchesM.Repositories.Interfaces;
+
+namespace LanchesM.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ LanchesM/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesM.Models;
+
+namespace LanchesM.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
